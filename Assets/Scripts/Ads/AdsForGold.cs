fc354c454ae6077d7e074cb4b57d9a7ce0262d96
Aleksey-Golub <55555257+Aleksey-Golub@users.noreply.github.com>
2020-05-14 19:37:15 +0200

    ADS
    add ADS

---
+++ Assets/Scripts/Ads/AdsForGold.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+public class AdsForGold : MonoBehaviour, IUnityAdsListener
+{
+    [SerializeField] private GameObject startWatchingButtom;
+
+    private string gameId = "3544550";
+    private string myPlacementId = "rewardedVideo";
+    private bool testMode = true;
+
+
+    void Start()
+    {
+        Advertisement.AddListener(this);
+        Advertisement.Initialize(gameId, testMode);
+
+        startWatchingButtom.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            Advertisement.Show(myPlacementId);
+            startWatchingButtom.SetActive(false);
+        });
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        // логика начисления награды
+        if (showResult == ShowResult.Finished)
+        {
+            Debug.Log("Вам начислено 100 монет!");
+        }
+        else if(showResult == ShowResult.Skipped)
+        {
+
+        }
+        else if (showResult == ShowResult.Failed)
+        {
+
+        }
+    }
+    public void OnUnityAdsReady(string placementId)
+    {
+        if (placementId == myPlacementId)
+        {
+            startWatchingButtom.SetActive(true);
+        }
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        // Н. логика остановки игры при запуске рекламы
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        // реклама завершилась с ошибкой
+    }
+}
---
+++ Assets/Scripts/Ads/AdsInit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsInit : MonoBehaviour
+{
+    private string gameId = "3544550"; // Id игры в кабиненте Unity в Settings->ProjectSettings отдельно для AppStore и GooglePlay
+    private bool testMode = true;
+
+    private void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady("DownBanner"))    // string placementId
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER); 
+
+        Advertisement.Banner.Show("DownBanner");    // string placementId, ро умолчанию вверху слева
+    }
+}
